6c510592fa704e388846a5d23ec5bf50efcddfe8
Luis Alonso Bogantes Rodriguez <ABogantesRodriguez@ina.ac.cr>
2020-07-01 22:03:47 -0600

    Creacion del proyecto

---
+++ Buscaminas_v1/Buscaminas_v1/Buscaminas_v1.cpp
@@ -0,0 +1,11 @@
+// Buscaminas_v1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Buscaminas v1\n";
+    system("pause");
+}
+
